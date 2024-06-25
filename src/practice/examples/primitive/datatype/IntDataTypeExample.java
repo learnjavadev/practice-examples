d8f3d5f0ae0fcbca76085ed35068f486382019e5
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 13:56:20 +0530

    Problem3 PrimitiveDataTypeExample Solution

---
+++ src/practice/examples/primitive/datatype/IntDataTypeExample.java
@@ -0,0 +1,25 @@
+
+public class IntDataTypeExample {
+	public static void main(String[] args) {
+		
+		int number1 = 10;
+		int number2 = 7;
+		 int addition = number1+number2;
+		 int substraction = number1-number2;
+		 int multiplication  = number1*number2;
+		 int division  = number1/ number2;
+		 int remainder = number1% number2;
+		 
+		 
+		 
+		 
+		System.out.println("Addition of int numbers is=" +number1+ "and" +number2+ "is"+addition);
+		System.out.println("substraction of int numbers is=" +number1+ "and" +number2+ "is"+substraction);
+		System.out.println("multiplication of int numbers is=" +number1+ "and" +number2+ "is" +multiplication);
+		System.out.println(" division of int numbers is=" +number1+ "and" +number2+ "is" +division );
+		System.out.println("remainder of int numbers is=" +number1+ "and" +number2+ "is" +remainder);
+		
+		
+	}
+
+}
