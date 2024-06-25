199189df15692261c8db12a28471890a8f835dcd
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 18:58:12 +0530

    problem3 NonPrimitiveDataTypeExample solution

---
+++ src/practice/examples/non/DoubleArrayExample.java
@@ -0,0 +1,19 @@
+
+public class DoubleArrayExample {
+
+	public static void main(String[] args) {
+
+		double[] doubleArray = new double[3];
+  		
+  		doubleArray[0] = 2.11;
+  		doubleArray[1] = 17.07;
+  		for(int i = 0; i< doubleArray.length ; i++) {
+  			
+  			System.out.println("array index=" +i+", value stored="  +doubleArray[i]);
+  			
+  			
+  		}
+
+	}
+
+}
