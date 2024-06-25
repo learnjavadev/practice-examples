87fa094598a0704724a61f72528bb83423346b80
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 13:54:56 +0530

    Problem2 PrimitiveDataTypeExample Solution

---
+++ src/practice/examples/primitive/datatype/BooleanDataTypeExample.java
@@ -0,0 +1,17 @@
+
+public class BooleanDataTypeExample {
+	public static void main(String[] args) {
+	boolean Variable1 = true;
+	boolean Variable2 = false;
+	
+	System.out.println("Value of boolean variable is="+Variable1);
+	System.out.println("Value of boolean variable is="+Variable2);
+	
+	System.out.println("End Of Example");	
+		
+		
+		
+		
+	}
+
+}
