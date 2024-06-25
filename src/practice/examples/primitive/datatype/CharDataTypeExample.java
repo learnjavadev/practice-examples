79b414bb3e755e1c0f82f6dcfe7e479ca4e2bd18
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 13:53:22 +0530

    Problem1 PrimitiveDataTypeExample Solution

---
+++ src/practice/examples/primitive/datatype/CharDataTypeExample.java
@@ -0,0 +1,14 @@
+
+public class CharDataTypeExample {
+	public static void main(String[] args) {
+		
+		char myChar = 'A';
+		
+		System.out.println("The Value Of myChar is:"+myChar);
+		
+		
+		
+	}
+	
+
+}
