de727e302a28794ef40f36d75eb30537251bf1ac
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 13:59:25 +0530

    Problem5 PrimitiveDataTypeExample Solution

---
+++ src/practice/examples/primitive/datatype/DoubleDataTypeExample.java
@@ -0,0 +1,34 @@
+
+public class DoubleDataTypeExample {
+	public static void main(String[] args) {
+		
+		double Number1 = 14.24;
+		double Number2 = 13.20;
+		
+		double additionresult = Number1 + Number2 ;
+		double substractionresult = Number1 - Number2 ;
+		double multiplicationresult = Number1 * Number2 ;
+		double divisionresult = Number1 / Number2 ;
+		
+		
+		System.out.println("Addition of double data type numbers" +Number1+ "and" +Number2+ "is=" +additionresult);
+		System.out.println("Substraction of double data type numbers" +Number1+ "and" +Number2+ "is=" +substractionresult);
+		System.out.println("Multiplication of double data type  numbers" +Number1+ "and" +Number2+ "is=" +multiplicationresult);
+		System.out.println("Division of double data type numbers" +Number1+ "and" +Number2+ "is=" +divisionresult);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
