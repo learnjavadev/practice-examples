3f4d4728d3aacd33413b606fa353932780c22cfd
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 18:44:26 +0530

    Problem1 NonPrimitiveDataTypeExample solution

---
+++ src/practice/examples/non/StringDataTypeExample.java
@@ -0,0 +1,17 @@
+
+public class StringDataTypeExample {
+public static void main(String[] args) {
+	
+	String str1 = "Hii";
+	String str2 = "Sir";
+	
+	System.out.println("String variabe str1=" +str1);
+	System.out.println("string variable str2=" +str2);
+	  
+	String concatenated = str1+ ":" +str2;
+	System.out.println("Concatenated string:" +concatenated);
+	
+	
+	
+     }
+}
