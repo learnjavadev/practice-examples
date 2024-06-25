d3ecf8b23267009f5cd6974d7f42824b7d0d7dd9
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 19:00:55 +0530

    Problem5 NonPrimitiveDataTypeExample solution

---
+++ src/practice/examples/non/StringArrayExample.java
@@ -0,0 +1,24 @@
+
+public class StringArrayExample {
+
+	public static void main(String[] args) {
+		String[] stringArray = new String[3];
+		
+		
+		stringArray[0] = "First String";
+		
+		stringArray[2] = "Second String";
+		
+		
+		for(int i = 0; i<stringArray.length; i++) {
+			
+			
+		System.out.println("array index=" + i +", value stored=" +stringArray[i]);	
+			
+		
+		}
+		
+		
+	}
+
+}
