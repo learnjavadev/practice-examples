b4164e11e7755511f41f85ee74dff90a0ad6c2b1
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 13:57:57 +0530

    problem4 PrimitiveDataTypeExample Solution

---
+++ src/practice/examples/primitive/datatype/FloatDataTypeExample.java
@@ -0,0 +1,23 @@
+
+public class FloatDataTypeExample {
+
+	public static void main(String[] args) {
+	Float Number1 = 10.5f;
+	Float Number2 = 4.2f;
+	
+	float additionresult = Number1 + Number2;
+	System.out.println("Addition of float numbers" +Number1+ "and" +Number2+ "is" +additionresult);
+	
+	float substractionresult = Number1- Number2;
+	System.out.println("Substraction of float numbers" +Number1+ "and" +Number2+ "is" +substractionresult);
+	
+	float multiplicationresult = Number1*Number2;
+	System.out.println("multiplication of float numbers" +Number1+ "and" +Number2+ "is" +multiplicationresult);
+	
+	float divisionresult = Number1/ Number2;
+	System.out.println("division of float numbers" +Number1+ "and" +Number2+ "is" +divisionresult);
+	
+	
+	}
+
+}
