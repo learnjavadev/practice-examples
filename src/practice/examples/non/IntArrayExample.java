0e25f0ca0ee5d500230c9365c509e0ad41d68328
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 18:56:43 +0530

    Problem2 NonPrimitiveDataTypeExample solution

---
+++ src/practice/examples/non/IntArrayExample.java
@@ -0,0 +1,23 @@
+
+public class IntArrayExample {
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[4];
+		intArray[0] = 4;
+		intArray[1] = 2;
+		intArray[2] = 10;
+		intArray[3] = 12;
+		
+		
+		for(int i = 0; i< intArray.length; i++) {
+			
+		System.out.println("array index =" +i+", value stored =" +intArray[i]);	
+			
+			
+			
+			
+		}
+				
+	}
+
+}
