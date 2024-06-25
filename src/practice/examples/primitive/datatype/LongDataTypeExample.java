26055d8f961caf30d8820feb50ce72454cb640ba
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 14:00:39 +0530

    Problem6 PrimitiveDataTypeExample Solution

---
+++ src/practice/examples/primitive/datatype/LongDataTypeExample.java
@@ -0,0 +1,30 @@
+
+public class LongDataTypeExample {
+ 
+	public static void main(String[] args) {
+		
+		
+	long Number1 = 1200000000l;
+	long Number2 = 900000000l;
+	
+	long additionresult = Number1+Number2;
+	long substractionresult = Number1-Number2;
+	long multiplicationresult = Number1*Number2;
+	long divisionresult = Number1 / Number2;
+	
+	System.out.println("Addition of long data type numbers" +Number1+ "and" +Number2+ "is=" +additionresult);
+	System.out.println("Substraction of long data type numbers" +Number1+ "and" +Number2+ "is=" +substractionresult);
+	System.out.println("Multiplication of long data type numbers" +Number1+ "and" +Number2+ "is=" +multiplicationresult);
+	System.out.println("division of long data type numbers" +Number1+ "and" +Number2+ "is=" +divisionresult);
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}	
+}
