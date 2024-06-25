e5b4747caa965bc6284d9faf5cd7950978947c62
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-25 18:59:23 +0530

    Problem4 NonPrimitiveDataTypeExample solution

---
+++ src/practice/examples/non/ByteArrayExample.java
@@ -0,0 +1,21 @@
+
+public class ByteArrayExample {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[3];
+		
+		byte bytevalue = 33;
+		intArray[0] = bytevalue;
+
+		for(int i = 0; i< intArray.length; i++) {
+			
+		System.out.println("array index=" +i+ ", value stored=" + intArray[i]);	
+			
+			
+		}
+		
+		
+	}
+
+}
