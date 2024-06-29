bfb1d4005798a614bf81d21c2f794ee18ba347ed
Abhidevil752 <devkarabhijit2@gmail.com>
2024-06-29 11:42:30 +0530

    IntegerMultipleOfFive Problem4 Solution

---
+++ src/practice/examples/ifelse/IntegerMultipleOfFive.java
@@ -0,0 +1,22 @@
+
+public class IntegerMultipleOfFive {
+
+	public static void main(String[] args) {
+		
+     int number = 30;
+     
+     if(number % 5 == 0) {
+    	 System.out.println(number+ " is a multiple of 5.");
+    	 
+    	 
+     }else {
+    	 System.out.println(number+"is ! amultile of 5.");
+    	 
+    	 
+    	 
+     }
+     
+     
+	}
+
+}
